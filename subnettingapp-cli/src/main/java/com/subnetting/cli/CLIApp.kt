9505655970faf07a204094e1c6e06cc4e87e9eba
Daniel Gyorffy <judalion777@gmail.com>
2021-10-06 13:25:42 +0200

    :construction: CLI app enhancements

--- subnettingapp-cli/src/main/java/com/subnetting/cli/CLIApp.kt
+++ subnettingapp-cli/src/main/java/com/subnetting/cli/CLIApp.kt
@@ -5,6 +5,7 @@ package com.subnetting.cli
 import com.subnetting.ipv4.IPV4Address
 import com.subnetting.ipv4.IPV4Mask
 import com.subnetting.ipv4.IPV4Subnet
+import com.subnetting.ipv4.IPV4SubnetPartitioner
 
 fun main() {
     println("-".repeat(50))
@@ -15,9 +16,35 @@ fun main() {
 
     val (ip, mask) = readLine()!!.split(" ")
 
-    val subnet = IPV4Subnet(IPV4Address(ip, IPV4Mask(mask)))
+    println("What do you want to do?")
+    println("")
+
+}
+
+private fun printBasicSubnetInfo(ip: String, mask: String) {
+    printBasicSubnetInfo(IPV4Address(ip, IPV4Mask(mask)))
+}
+
+private fun printBasicSubnetInfo(ip: IPV4Address) {
+    printBasicSubnetInfo(IPV4Subnet(ip))
+}
+
+private fun printBasicSubnetInfo(subnet: IPV4Subnet) {
     println("Subnet address: ${subnet.subnetAddress}")
     println("First host address: ${subnet.firstHostAddress}")
     println("Last host address: ${subnet.lastHostAddress}")
     println("Broadcast address: ${subnet.broadcastAddress}")
+}
+
+private fun printSubnetPartitions(ip: String, mask: String, hostCounts: List<Int>) {
+    printSubnetPartitions(IPV4Address(ip, IPV4Mask(mask)), hostCounts)
+}
+
+private fun printSubnetPartitions(ip: IPV4Address, hostCounts: List<Int>) {
+    val partitioner = IPV4SubnetPartitioner(ip, hostCounts)
+    partitioner.subnets.forEach {
+        println("-".repeat(10))
+        printBasicSubnetInfo(it)
+        println()
+    }
 }

8e1e0b22f8a354cdf30955ac25e2b279ae48f28f
Daniel Gyorffy <judalion777@gmail.com>
2021-10-07 11:23:54 +0200

    :construction: Improving subnet partitioning

--- subnettingapp-core/src/main/java/com/subnetting/ipv4/IPV4SubnetPartitioner.kt
+++ subnettingapp-core/src/main/java/com/subnetting/ipv4/IPV4SubnetPartitioner.kt
@@ -2,11 +2,20 @@ package com.subnetting.ipv4
 
 import kotlin.math.pow
 
-class IPV4SubnetPartitioner(val baseIp: IPV4Address, val hostCounts: List<Int>) {
+class IPV4SubnetPartitioner(baseIp: IPV4Address, val hostCounts: List<Int>) {
 
-    val subnets: List<IPV4Subnet> = requiredMasks.map {
-        IPV4Subnet(IPV4Address(baseIp.octets, it))
-    }
+    private val baseIp = IPV4Subnet(baseIp).subnetAddress
+
+    val subnets: List<IPV4Subnet>
+        get() {
+            var lastSubnet: IPV4Subnet? = null
+            return requiredMasks.map { mask ->
+                when (lastSubnet) {
+                    null -> IPV4Subnet(IPV4Address(this.baseIp.octets, mask))
+                    else -> IPV4Subnet(IPV4Address(lastSubnet!!.broadcastAddress.next().octets, mask))
+                }.also { lastSubnet = it }
+            }
+        }
 
     private val requiredMasks: List<IPV4Mask>
         get() = hostCounts.map { IPV4Mask(32.minus(it.findAppropriatePower()).toInt()) }
@@ -22,6 +31,24 @@ class IPV4SubnetPartitioner(val baseIp: IPV4Address, val hostCounts: List<Int>) 
 
     private fun IPV4Address(octets: List<UByte>, mask: IPV4Mask): IPV4Address {
         return IPV4Address(octets[0], octets[1], octets[2], octets[3], mask)
+    }
+
+    private fun IPV4Address.next(): IPV4Address {
+        val octetStrings = stringFormat.split(".").toMutableList()
+        for (i in octetStrings.size.minus(1) downTo 0) {
+            val item = octetStrings[i].toInt()
+            if (item < 255) {
+                octetStrings[i] = item.plus(1).toString()
+                for (j in (i + 1) until 4) {
+                    octetStrings[j] = "0"
+                }
+                break
+            }
+        }
+
+        return IPV4Address(octetStrings.map(String::toUByte), mask).also {
+            println("The next ip to '${this.stringFormat}' is '${it.stringFormat}")
+        }
     }
 
 }

ef2d332fb4cad6578877de3ece0ba49c89ad1013
Daniel Gyoerffy <judalion777@gmail.com>
2022-08-08 17:01:33 +0200

    :sparkles: :lipstick: Live-time validation preview in the form

--- subnettingapp-web/src/main/resources/static/scripts/form.js
+++ subnettingapp-web/src/main/resources/static/scripts/form.js
@@ -1,13 +1,11 @@
-const SP = "sp";
-const SI = "si";
-
 window.onload = function () {
     initFieldDisablingBehaviour();
+    initValueCheckingPolicy();
 }
 
 function initFieldDisablingBehaviour() {
-    let hostsCountField = document.getElementById("hostsCount");
-    let radios = document.querySelectorAll("input[type='radio']");
+    const hostsCountField = document.getElementById("hostsCount");
+    const radios = document.querySelectorAll("input[type='radio']");
 
     const listener = function() {
         console.log("Radio clicked");
@@ -21,4 +19,34 @@ function initFieldDisablingBehaviour() {
     });
     
     listener();
+}
+
+function initValueCheckingPolicy() {
+    const ipPattern = new RegExp("\\d+\\.\\d+\\.\\d+\\.\\d+");
+    const cDirPattern = new RegExp("/\\d+");
+    const listPattern = new RegExp("(\\d+)(,\\s*\\d+)*");
+
+    const ipField = document.getElementById("ipField");
+    const maskField = document.getElementById("maskField");
+    const hostsCountField = document.getElementById("hostsCount");
+
+    
+    const operate = (field, predicate) => {
+        let validContent = predicate(field.value);
+        if (!validContent) field.classList.add("error");
+        else field.classList.remove("error");
+    };
+
+    const ipFieldListener = () => { operate(ipField, it => match(ipPattern, it)); };
+    const maskFieldListener = () => { operate(maskField, it => (match(ipPattern, it) || match(cDirPattern, it))); };
+    const hostsCountFieldListener = () => { operate(hostsCountField, it => match(listPattern, it)); };
+
+    ipField.addEventListener('input', ipFieldListener);
+    maskField.addEventListener('input', maskFieldListener);
+    hostsCountField.addEventListener('input', hostsCountFieldListener);
+}
+
+function match(regex, value) {
+    const parts = regex.exec(value);
+    return parts != null && parts[0] == value;
 }

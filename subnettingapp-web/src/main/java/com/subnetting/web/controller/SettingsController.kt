1f9c6d8a47f6555d57ddf19cf54030b4ca546d83
Daniel Gyoerffy <judalion777@gmail.com>
2022-08-17 15:33:19 +0200

    :ambulance: Compiler error

--- subnettingapp-web/src/main/java/com/subnetting/web/controller/SettingsController.kt
+++ subnettingapp-web/src/main/java/com/subnetting/web/controller/SettingsController.kt
@@ -1,7 +1,6 @@
 package com.subnetting.web.controller
 
 import com.subnetting.web.settings.AppSettings
-import com.subnetting.web.settings.ConfigEntries
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping

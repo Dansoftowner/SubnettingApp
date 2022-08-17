ab94ce52c9dd8e84827ca0444d9f990d51bd8050
Daniel Gyoerffy <judalion777@gmail.com>
2022-08-17 20:02:26 +0200

    :wrench: Making subnettingapp-web available for deploying

--- subnettingapp-web/src/main/java/com/subnetting/web/SubnettingWebApplication.kt
+++ subnettingapp-web/src/main/java/com/subnetting/web/SubnettingWebApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class SubnettingWebApplication
+class SubnettingWebApplication
 
 fun main(args: Array<String>) {
     runApplication<SubnettingWebApplication>(*args)

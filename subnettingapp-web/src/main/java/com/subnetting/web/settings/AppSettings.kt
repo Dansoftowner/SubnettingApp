a2fed8d16db3c96cd1c280ee08c91bf5f3fcbf12
Daniel Gyoerffy <judalion777@gmail.com>
2022-08-22 19:49:15 +0200

    :bug: App crashes if empty cookies received

--- subnettingapp-web/src/main/java/com/subnetting/web/settings/AppSettings.kt
+++ subnettingapp-web/src/main/java/com/subnetting/web/settings/AppSettings.kt
@@ -1,3 +1,21 @@
+/*
+ * SubnettingApp
+ * Copyright (c) 2022.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.subnetting.web.settings
 
 import com.subnetting.web.settings.ConfigEntry.Option
@@ -26,9 +44,10 @@ class AppSettings(request: HttpServletRequest, private val configEntries: List<C
         request.cookies
             ?.filter { configEntries.configNames.contains(it.name) }
             ?.associate { it.name to it.value }
-            ?.mapKeys { it -> configEntries.toConfigEntry(it.key)!! }
-            ?.mapValues { entry -> entry.key.toOption(entry.value)!! }
-            ?.forEach { this[it.key] = it.value }
+            ?.mapKeys { it -> configEntries.toConfigEntry(it.key) }
+            ?.mapValues { entry -> entry.key?.toOption(entry.value) }
+            ?.filter { it.key != null && it.value != null }
+            ?.forEach { this[it.key!!] = it.value!! }
     }
 
     operator fun get(entryName: String) = this[configEntries.toConfigEntry(entryName)]

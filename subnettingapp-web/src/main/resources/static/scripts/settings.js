4576360aed4f94e546143b62eeefce0727f522b0
Daniel Gyoerffy <judalion777@gmail.com>
2022-08-23 17:08:24 +0200

    :bug: Save button is disabled even if a checkbox is modified in settings page

--- subnettingapp-web/src/main/resources/static/scripts/settings.js
+++ subnettingapp-web/src/main/resources/static/scripts/settings.js
@@ -2,7 +2,7 @@ window.onload = function() {
     let saveButton = document.querySelector("input[type='submit']");
     saveButton.disabled = true;
 
-    document.querySelectorAll("select").forEach((it) => {
+    document.querySelectorAll("select, input").forEach((it) => {
         it.addEventListener("change", () => {
             saveButton.disabled = false;
         });
